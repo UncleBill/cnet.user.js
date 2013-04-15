319dcf0cf5c47949efbca344f02150e10d8a083b
UncleBill <billbill290@gmail.com>
2013-04-15 11:51:32 +0800

    store account & password

--- cnet.user.js
+++ cnet.user.js
@@ -10,14 +10,60 @@
 
 
 (function(){
+
+    var ultis = {
+        $id : function( id ){
+            return document.getElementById( id );
+        },
+    }
+
+
+    console.log('loaded');
+
     var _ACCOUNT_ = "01124611158";
     var _PASSWORD_ = "329225";
 
-    var account = document.getElementById("account");
-    var password = document.getElementById("userPassword");
+    var storedAccount = localStorage.getItem("account");
+    var storedPassword = localStorage.getItem("password");
+
+    console.log('is localStorage',storedAccount,storedPassword);
+    if( storedAccount || storedPassword ){
+        _ACCOUNT_ = storedAccount;
+        _PASSWORD_ = storedPassword;
+    }
+
+    var account = ultis.$id("account");
+    var password = ultis.$id("userPassword");
     if( account ){
         account.value = _ACCOUNT_;
         password.value = _PASSWORD_;
         console.log( 'done!' );
     }
+
+    var radio = document.createElement("input");
+    var label = document.createElement("label");
+    radio.setAttribute("type","checkbox");
+    radio.setAttribute("name","member");
+    radio.setAttribute("id","member");
+    label.setAttribute("for","member");
+    label.textContent = "记住密码";
+
+    var parent = password.parentElement;
+    parent.appendChild(radio);
+    parent.appendChild(label);
+
+    var logBtn = ultis.$id("login_button");
+    logBtn.addEventListener("click",function(){
+        var ismember = ultis.$id("member");
+        if( isMember.checked ){
+            localStorage['account'] = account.value;
+            localStorage['password'] = password.value;
+        } else {
+            localStorage['account'] = "";
+            localStorage['password'] = "";
+        }
+    })
+    console.log( 'localStorage:','account:',localStorage['account'],'password',localStorage['password'] );
+
+
 }()); // closure
